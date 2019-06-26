d31a8a49acce0c519440d6ee5d383e7b9f987b3e
Manjuguna <52068092+Manjuguna@users.noreply.github.com>
2019-06-26 15:40:52 +0530

    Update gm4.py

--- gm4.py
+++ gm4.py
@@ -1,4 +1,4 @@
-ch=input("please Enter your own character:")
+ch=input("Please Enter Your Own Character:")
 if(ch=='a' or ch=='e' or ch=='i' or ch=='o' or ch=='u' or ch=='A' or ch=='E' or ch=='I' or ch=='O' or ch=='U'):
      print("thr given character",ch,"is a vowel")
  else:
