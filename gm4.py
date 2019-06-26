4793f36abfc6f4cab4cf9ec929d775f05d47fb68
Manjuguna <52068092+Manjuguna@users.noreply.github.com>
2019-06-26 15:38:32 +0530

    Create gm4.py

---
+++ gm4.py
@@ -0,0 +1,5 @@
+ch=input("please Enter your own character:")
+if(ch=='a' or ch=='e' or ch=='i' or ch=='o' or ch=='u' or ch=='A' or ch=='E' or ch=='I' or ch=='O' or ch=='U'):
+     print("thr given character",ch,"is a vowel")
+ else:
+     print("the given character",ch,"is constant")
